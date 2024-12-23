1cd1698a8c35438320a9b0247737581d2d76c9a2
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-12-23 19:25:09 -0300

    Initial commit

---
+++ BlazorComponent/Repositories/MedicoRepository.cs
@@ -0,0 +1,19 @@
+﻿using BlazorComponent.Models;
+
+namespace BlazorComponent.Repositories
+{
+    public class MedicoRepository : IMedicoRepository
+    {
+        public async Task<List<Medico>> GetAllAsync()
+        {
+            return new List<Medico>
+            { 
+                new Medico{ Id = 1, Nome = "João Gabriel", Crm = "ABCD123"},
+                new Medico{ Id = 2, Nome = "João Gabriel", Crm = "ABCD123"},
+                new Medico{ Id = 3, Nome = "João Gabriel", Crm = "ABCD123"},
+                new Medico{ Id = 4, Nome = "João Gabriel", Crm = "ABCD123"},
+                new Medico{ Id = 5, Nome = "João Gabriel", Crm = "ABCD123"},
+            };
+        }
+    }
+}
