5269dbdf1dba88ddbac721d3e1ddf9fa438db3d3
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-12-23 20:02:03 -0300

    Adding validations

--- BlazorComponent/Models/Medico.cs
+++ BlazorComponent/Models/Medico.cs
@@ -1,9 +1,17 @@
-﻿namespace BlazorComponent.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorComponent.Models
 {
     public class Medico
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Nome deve ser Fornecido")]
+        [MinLength(3, ErrorMessage = "Nome deve conter no mínimo 3 caracteres")]
+        [MaxLength(50, ErrorMessage = "Nome deve conter no máximo 50 caracteres")]
+        //[Range(3,50, ErrorMessage = "Nome deve conter entre {0} e {1} caracteres")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "CRM deve ser Fornecido")]
         public string Crm { get; set; }
     }
 }
